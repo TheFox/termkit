0b9a446d7ecbd655f8940dcfd8ab310f315d0cf2
Christian Mayer <christian@fox21.at>
2016-07-24 17:22:57 +0200

    Version 0.0.0.

--- lib/termkit/version.rb
+++ lib/termkit/version.rb
@@ -1,8 +1,8 @@
 
 module TheFox
 	module TermKit
-		RELEASE_ID = 0
-		VERSION = '0.0.0-dev'
+		RELEASE_ID = 1
+		VERSION = '0.0.0'
 		DATE = '2016-07-24'
 		HOMEPAGE = 'https://github.com/TheFox/termkit'
 	end
