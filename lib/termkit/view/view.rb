64688f69de16406af2afbe8fb3aab7e7bb687bbe
Christian Mayer <christian@fox21.at>
2016-09-16 13:41:17 +0200

    Draw point. [1]

--- lib/termkit/view/view.rb
+++ lib/termkit/view/view.rb
@@ -16,7 +16,7 @@ module TheFox
 			attr_accessor :grid
 			attr_accessor :grid_cache
 			attr_reader :position
-			attr_accessor :zindex
+			attr_reader :zindex
 			
 			def initialize(name = nil)
 				#puts 'View->initialize'
@@ -29,7 +29,28 @@ module TheFox
 				
 				@is_visible = false
 				@position = Point.new(0, 0)
-				@zindex = nil
+				@zindex = 1
+			end
+			
+			##
+			# FOR DEBUG ONLY
+			def pp_grid
+				@grid.map{ |y_pos, row|
+					[y_pos, row.map{ |x_pos, content| [x_pos, content.char] }.to_h]
+				}.to_h
+			end
+			
+			##
+			# FOR DEBUG ONLY
+			def pp_grid_cache
+				@grid_cache.map{ |y_pos, row|
+					[y_pos,
+						row.map{ |x_pos, content|
+							# [x_pos, {'c' => content.char, 'v' => content.view.name}]
+							[x_pos, content.char]
+						}.to_h,
+					]
+				}.to_h
 			end
 			
 			def is_visible=(is_visible)
@@ -58,27 +79,69 @@ module TheFox
 				@position = position
 			end
 			
-			def add_subview(view)
-				if view == self
-					raise ArgumentError, 'self given'
-				end
-				if !view.is_a?(View)
-					raise ArgumentError, "Argument is not a View -- #{view.class} given"
-				end
+			def zindex=(zindex)
+				@zindex = zindex
 				
-				view.parent_view = self
-				@subviews.push(view)
+				puts "#{@name} -- set zindex #{zindex} p=#{@parent_view.nil? ? 'N' : 'Y'}"
+				
+				if !@parent_view.nil?
+					@grid_cache.each do |y_pos, row|
+						row.each do |x_pos, content|
+							point = Point.new(x_pos + @position.x, y_pos + @position.y)
+							
+							puts "#{@name} -- set zindex #{zindex}, #{point.x}:#{point.y}"
+							
+							@parent_view.redraw_zindex(point)
+						end
+					end
+				end
 			end
 			
-			def remove_subview(view)
-				if view == self
+			def add_subview(subview)
+				if subview == self
 					raise ArgumentError, 'self given'
 				end
-				if !view.is_a?(View)
-					raise ArgumentError, "Argument is not a View -- #{view.class} given"
+				if !subview.is_a?(View)
+					raise ArgumentError, "Argument is not a View -- #{subview.class} given"
 				end
 				
-				@subviews.delete(view)
+				subview.parent_view = self
+				r_subview = @subviews.push(subview)
+				
+				subview.grid_cache.each do |y_pos, row|
+					row.each do |x_pos, content|
+						point = Point.new(x_pos + subview.position.x, y_pos + subview.position.y)
+						
+						puts "#{@name} -- add_subview, redraw_zindex #{point.x}:#{point.y}"
+						
+						redraw_zindex(point)
+					end
+				end
+				
+				r_subview
+			end
+			
+			def remove_subview(subview)
+				if subview == self
+					raise ArgumentError, 'self given'
+				end
+				if !subview.is_a?(View)
+					raise ArgumentError, "Argument is not a View -- #{subview.class} given"
+				end
+				
+				r_subview = @subviews.delete(subview)
+				
+				subview.grid_cache.each do |y_pos, row|
+					row.each do |x_pos, content|
+						point = Point.new(x_pos + subview.position.x, y_pos + subview.position.y)
+						
+						puts "#{@name} -- remove_subview, grid_cache_erase_point #{point.x}:#{point.y}"
+						
+						grid_cache_erase_point(point)
+					end
+				end
+				
+				r_subview
 			end
 			
 			##
@@ -107,7 +170,7 @@ module TheFox
 				
 				
 				puts
-				puts "#{@name} -- draw"
+				puts "#{@name} -- draw '#{content}'"
 				
 				
 				if is_foreign_point
@@ -121,44 +184,48 @@ module TheFox
 					@grid[y_pos][x_pos] = content
 				end
 				
-				puts "#{@name} -- draw #{x_pos}:#{y_pos} foreign=#{is_foreign_point ? 'Y' : 'N'} from=#{content.view.name}"
+				new_point = Point.new(x_pos, y_pos)
 				
-				
-				if !@grid_cache[y_pos]
-					@grid_cache[y_pos] = {}
-				end
+				puts "#{@name} -- draw '#{content}' #{x_pos}:#{y_pos} foreign=#{is_foreign_point ? 'Y' : 'N'} from=#{content.view.name}"
 				
 				puts "#{@name} -- subviews: #{@subviews.count}"
 				
 				changed = false
 				
 				if @subviews.count == 0
-					@grid_cache[y_pos][x_pos] = content
-					changed = true
+					changed = set_grid_cache(new_point, content)
 				else
 					puts "#{@name} -- has subviews"
 					
-					if @grid_cache[y_pos][x_pos]
-						puts "#{@name} -- search subviews"
+					if @grid_cache[y_pos] && @grid_cache[y_pos][x_pos]
+						puts "#{@name} -- found something on cached grid"
 						
-						redraw_zindex(Point.new(x_pos, y_pos))
+						redraw_zindex(new_point)
 					else
 						puts "#{@name} -- draw free point"
-						@grid_cache[y_pos][x_pos] = content
-						changed = true
+						changed = set_grid_cache(new_point, content)
 					end
 				end
 				
-				if changed && is_visible? && !@parent_view.nil?
-					puts "#{@name} -- draw parent: #{@parent_view.name}"
-					@parent_view.draw_point(point, content)
+				if changed
+					parent_draw_point(new_point, content)
 				end
 				
 				return true
 			end
 			
+			def parent_draw_point(point, content)
+				if !@parent_view.nil? && is_visible?
+					
+					#point
+					
+					puts "#{@name} -- draw parent: #{@parent_view.name} #{point.x}:#{point.y}"
+					@parent_view.draw_point(point, content)
+				end
+			end
+			
 			##
-			# Redraw to Parent View based on the visibility trend of +is_visible=+.
+			# Redraw to Parent View based on the visibility trend.
 			def redraw_parent(visibility_trend)
 				puts "#{@name} -- redraw parent, #{visibility_trend}"
 				
@@ -190,8 +257,6 @@ module TheFox
 									@parent_view.grid_cache_erase_point(point)
 								else
 									puts "#{@name} -- redraw parent, -1, #{parent_x_pos}:#{parent_y_pos}, not the same"
-									
-									
 								end
 							end
 						end
@@ -203,10 +268,10 @@ module TheFox
 			##
 			# Erase a single Point of the Grid Cache.
 			def grid_cache_erase_point(point)
-				puts "#{@name} -- erase point, #{point}"
-				
 				x_pos = point.x
 				y_pos = point.y
+				
+				puts "#{@name} -- erase point, #{x_pos}:#{y_pos}"
 				
 				if @grid_cache[y_pos] && @grid_cache[y_pos][x_pos]
 					#@grid_cache[y_pos][x_pos] = nil
@@ -220,13 +285,105 @@ module TheFox
 			# Redraw a single Point based on the zindexes of the subviews.
 			# Happens when a subview added, removed, hides, or zindex changes.
 			def redraw_zindex(point)
-				puts "#{@name} -- redraw zindex, #{point}"
+				x_pos = point.x
+				y_pos = point.y
 				
-				pp @subviews.map{ |subview| subview.zindex }
+				puts "#{@name} -- redraw zindex #{x_pos}:#{y_pos}"
+				
+				views = @subviews
+					.select{ |subview| subview.is_visible? }
+					.select{ |subview| subview.zindex >= 1 }
+					.select{ |subview|
+						
+						subview_x_pos = x_pos - subview.position.x
+						subview_y_pos = y_pos - subview.position.y
+						
+						content = subview.grid_cache[subview_y_pos] && subview.grid_cache[subview_y_pos][subview_x_pos]
+						
+						# puts "#{@name} -- find #{subview_x_pos}:#{subview_y_pos} on cached grid in '#{subview}': '#{content}'"
+						
+						!content.nil?
+					}
+					.sort{ |subview1, subview2| subview1.zindex <=> subview2.zindex }
+				
+				pp views.map{ |subview| subview.name }
+				
+				view = views.last
+				
+				changed = false
+				content = nil
+				
+				if view.nil?
+					# When no subview was found draw the current view,
+					# if a point on the current view's grid exist.
+					
+					puts "#{@name} -- redraw zindex, no view"
+					
+					if @grid[y_pos] && @grid[y_pos][x_pos]
+						puts "#{@name} -- redraw zindex, found something on the grid: '#{@grid[y_pos][x_pos]}'"
+						content = @grid[y_pos][x_pos]
+						changed = set_grid_cache(point, content)
+					else
+						puts "#{@name} -- redraw zindex, nothing on the grid"
+					end
+				else
+					puts "#{@name} -- redraw zindex, last view: '#{view}'"
+					
+					if @grid_cache[y_pos] && @grid_cache[y_pos][x_pos]
+						subview_x_pos = x_pos - view.position.x
+						subview_y_pos = y_pos - view.position.y
+						
+						content = view.grid_cache[subview_y_pos][subview_x_pos]
+						
+						puts "#{@name} -- redraw zindex, view '#{view}', point #{subview_x_pos}:#{subview_y_pos}: '#{content}'"
+						
+						changed = set_grid_cache(point, content)
+					else
+						puts "#{@name} -- redraw zindex, nothing on the cached grid"
+					end
+				end
+				
+				if changed
+					puts "#{@name} -- redraw zindex, changed"
+					parent_draw_point(point, content)
+				end
+				
+				changed
+			end
+			
+			def set_grid_cache(point, content)
+				x_pos = point.x
+				y_pos = point.y
+				
+				if !@grid_cache[y_pos]
+					@grid_cache[y_pos] = {}
+				end
+				
+				changed = if @grid_cache[y_pos][x_pos]
+						if @grid_cache[y_pos][x_pos] == content
+							false
+						else
+							true
+						end
+					else
+						true
+					end
+				
+				if changed
+					@grid_cache[y_pos][x_pos] = content
+				end
+				
+				puts "#{@name} -- set grid #{x_pos}:#{y_pos} '#{content}' changed=#{changed ? 'Y' : 'N'}"
+				
+				changed
 			end
 			
 			def render
 				
+			end
+			
+			def to_s
+				@name
 			end
 			
 		end
--- test/test_view.rb
+++ test/test_view.rb
@@ -43,6 +43,12 @@ class TestView < MiniTest::Test
 		assert_equal(false, view1.is_visible?)
 	end
 	
+	def test_position_exception
+		view1 = View.new
+		
+		assert_raises(ArgumentError){ view1.position = 'INVALID' }
+	end
+	
 	def test_add_subview
 		view1 = View.new
 		view2 = View.new
@@ -67,6 +73,7 @@ class TestView < MiniTest::Test
 	def test_add_subview_exception
 		view1 = View.new
 		
+		assert_raises(ArgumentError){ view1.add_subview(view1) }
 		assert_raises(ArgumentError){ view1.add_subview('INVALID') }
 	end
 	
@@ -97,10 +104,11 @@ class TestView < MiniTest::Test
 	def test_remove_subview_exception
 		view1 = View.new
 		
+		assert_raises(ArgumentError){ view1.remove_subview(view1) }
 		assert_raises(ArgumentError){ view1.remove_subview('INVALID') }
 	end
 	
-	def test_draw_point
+	def test_draw_point_simple
 		puts
 		puts '-- Simple ----------'
 		
@@ -110,12 +118,9 @@ class TestView < MiniTest::Test
 		assert_equal(true, view1.draw_point([0, 0], 'A'))
 		assert_equal('A', view1.grid[0][0].char)
 		assert_equal('A', view1.grid_cache[0][0].char)
-		
-		
-		puts
-		puts '-- Subviews --------'
-		
-		# Subviews
+	end
+	
+	def test_draw_point_subviews_base
 		view1 = View.new('view1')
 		view1.is_visible = true
 		
@@ -132,8 +137,116 @@ class TestView < MiniTest::Test
 		
 		assert_equal('A', view1.grid_cache[0][0].char)
 		assert_equal('B', view1.grid_cache[0][1].char)
+	end
+	
+	def test_draw_point_subviews_chain1
+		view1 = View.new('view1')
+		view1.is_visible = true
 		
+		view2 = View.new('view2')
+		view2.position = Point.new(1, 0)
+		view2.is_visible = true
+		view1.add_subview(view2)
 		
+		view3 = View.new('view3')
+		view3.position = Point.new(1, 0)
+		view3.is_visible = true
+		view2.add_subview(view3)
+		
+		# assert_equal(true, view1.draw_point([0, 0], 'A'))
+		# assert_equal(true, view1.draw_point([1, 0], 'A'))
+		# assert_equal(true, view2.draw_point([0, 0], 'B'))
+		# assert_equal(true, view2.draw_point([1, 0], 'B'))
+		assert_equal(true, view3.draw_point([0, 0], 'C'))
+		
+		# pp view1.pp_grid_cache
+		# pp view2.pp_grid_cache
+		# pp view3.pp_grid_cache
+		
+		assert_equal('C', view3.grid_cache[0][0].char)
+		assert_equal('C', view2.grid_cache[0][1].char)
+		assert_equal('C', view1.grid_cache[0][2].char)
+	end
+	
+	def test_draw_point_subviews_chain2
+		view1 = View.new('view1')
+		view1.is_visible = true
+		
+		view2 = View.new('view2')
+		view2.position = Point.new(1, 0)
+		view2.is_visible = true
+		view1.add_subview(view2)
+		
+		view3 = View.new('view3')
+		view3.position = Point.new(1, 0)
+		view3.is_visible = true
+		view2.add_subview(view3)
+		
+		assert_equal(true, view1.draw_point([0, 0], 'A'))
+		assert_equal(true, view1.draw_point([1, 0], 'A'))
+		assert_equal(true, view2.draw_point([0, 0], 'B'))
+		assert_equal(true, view2.draw_point([1, 0], 'B'))
+		assert_equal(true, view3.draw_point([0, 0], 'C'))
+		
+		# pp view1.pp_grid_cache
+		# pp view2.pp_grid_cache
+		# pp view3.pp_grid_cache
+		
+		assert_equal('C', view3.grid_cache[0][0].char)
+		assert_equal('B', view2.grid_cache[0][0].char)
+		assert_equal('C', view2.grid_cache[0][1].char)
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('C', view1.grid_cache[0][2].char)
+	end
+	
+	def test_draw_point_subviews_chain3
+		view1 = View.new('view1')
+		view1.is_visible = true
+		
+		view2 = View.new('view2')
+		view2.position = Point.new(1, 0)
+		view2.is_visible = true
+		view1.add_subview(view2)
+		
+		view3 = View.new('view3')
+		view3.position = Point.new(1, 0)
+		view3.is_visible = true
+		view2.add_subview(view3)
+		
+		assert_equal(true, view3.draw_point([0, 0], 'C'))
+		
+		# pp view1.pp_grid_cache
+		# pp view2.pp_grid_cache
+		# pp view3.pp_grid_cache
+		
+		assert_equal('C', view3.grid_cache[0][0].char)
+		assert_equal('C', view2.grid_cache[0][1].char)
+		assert_equal('C', view1.grid_cache[0][2].char)
+		
+		assert_equal(true, view2.draw_point([1, 0], 'B'))
+		assert_equal(true, view2.draw_point([0, 0], 'B'))
+		
+		# pp view1.pp_grid_cache
+		# pp view2.pp_grid_cache
+		# pp view3.pp_grid_cache
+		
+		assert_equal('C', view3.grid_cache[0][0].char)
+		assert_equal('C', view2.grid_cache[0][1].char)
+		assert_equal('C', view1.grid_cache[0][2].char)
+		
+		assert_equal('B', view2.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		
+		assert_equal(true, view1.draw_point([1, 0], 'A'))
+		assert_equal(true, view1.draw_point([0, 0], 'A'))
+		
+		# pp view1.pp_grid_cache
+		# pp view2.pp_grid_cache
+		# pp view3.pp_grid_cache
+	end
+	
+	def test_draw_point_visible_false
 		puts
 		puts '-- Visible false ---'
 		
@@ -153,8 +266,9 @@ class TestView < MiniTest::Test
 		
 		assert_equal('A', view1.grid_cache[0][0].char)
 		assert_nil(view1.grid_cache[0][1])
-		
-		
+	end
+	
+	def test_draw_point_visible_true
 		puts
 		puts '-- Visible true ----'
 		
@@ -188,11 +302,233 @@ class TestView < MiniTest::Test
 		
 		assert_equal('A', view1.grid_cache[0][0].char)
 		assert_nil(view1.grid_cache[0][1])
-		
+	end
+	
+	def test_draw_point_zindex_base
 		puts
-		puts '--------------------'
+		puts '-- Zindex ----------'
+		
+		view1 = View.new('view1')
+		view1.is_visible = true
+		
+		view3 = View.new('view3')
+		view3.position = Point.new(2, 0)
+		view3.is_visible = true
+		view3.zindex = 2
+		view1.add_subview(view3)
+		
+		view2 = View.new('view2')
+		view2.position = Point.new(1, 0)
+		view2.is_visible = true
+		view2.zindex = 1
+		view1.add_subview(view2)
+		
+		view4 = View.new('view4')
+		view4.position = Point.new(2, 0)
+		view4.is_visible = true
+		view4.zindex = 3
+		view1.add_subview(view4)
+		
+		assert_equal(true, view1.draw_point([0, 0], 'A'))
+		assert_equal(true, view2.draw_point([0, 0], 'B'))
+		assert_equal(true, view2.draw_point([1, 0], 'C'))
+		assert_equal(true, view3.draw_point([0, 0], 'D'))
+		
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('D', view1.grid_cache[0][2].char)
+	end
+	
+	def test_draw_point_zindex_change1
+		view1 = View.new('view1')
+		view1.is_visible = true
+		
+		view2 = View.new('view2')
+		view2.position = Point.new(1, 0)
+		view2.is_visible = true
+		view2.zindex = 10
+		view1.add_subview(view2)
+		
+		view3 = View.new('view3')
+		view3.position = Point.new(2, 0)
+		view3.is_visible = true
+		view3.zindex = 20
+		view1.add_subview(view3)
+		
+		assert_equal(true, view1.draw_point([0, 0], 'A'))
+		assert_equal(true, view1.draw_point([1, 0], 'A'))
+		assert_equal(true, view1.draw_point([2, 0], 'A'))
+		assert_equal(true, view2.draw_point([0, 0], 'B'))
+		assert_equal(true, view2.draw_point([1, 0], 'B'))
+		assert_equal(true, view3.draw_point([0, 0], 'C'))
+		
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('C', view1.grid_cache[0][2].char)
 		
 		
+		
+		# pp view1.pp_grid_cache
+		
+		view3.zindex = 10
+		
+		# pp view1.pp_grid_cache
+		
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('C', view1.grid_cache[0][2].char)
+		
+		view3.zindex = 5
+		
+		# pp view1.pp_grid_cache
+		
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('B', view1.grid_cache[0][2].char)
+	end
+	
+	def test_draw_point_zindex_change2
+		view1 = View.new('view1')
+		view1.is_visible = true
+		
+		view2 = View.new('view2')
+		view2.position = Point.new(1, 0)
+		view2.is_visible = true
+		view1.add_subview(view2)
+		
+		view3 = View.new('view3')
+		view3.position = Point.new(1, 0)
+		view3.is_visible = true
+		view3.zindex = 10
+		view2.add_subview(view3)
+		
+		view4 = View.new('view4')
+		view4.position = Point.new(1, 0)
+		view4.is_visible = true
+		view4.zindex = 20
+		view2.add_subview(view4)
+		
+		assert_equal(true, view1.draw_point([0, 0], 'A'))
+		assert_equal(true, view1.draw_point([1, 0], 'A'))
+		assert_equal(true, view1.draw_point([2, 0], 'A'))
+		# assert_equal(true, view1.draw_point([3, 0], 'A'))
+		assert_equal(true, view2.draw_point([0, 0], 'B'))
+		assert_equal(true, view2.draw_point([1, 0], 'B'))
+		assert_equal(true, view2.draw_point([2, 0], 'B'))
+		# assert_equal(true, view2.draw_point([3, 0], 'B'))
+		assert_equal(true, view3.draw_point([0, 0], 'C'))
+		assert_equal(true, view4.draw_point([0, 0], 'D'))
+		assert_equal(true, view4.draw_point([1, 0], 'D'))
+		
+		# pp view1.pp_grid_cache
+		
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('D', view1.grid_cache[0][2].char)
+		assert_equal('D', view1.grid_cache[0][3].char)
+		
+		view4.zindex = 5
+		
+		# pp view1.pp_grid_cache
+		
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('C', view1.grid_cache[0][2].char)
+		assert_equal('D', view1.grid_cache[0][3].char)
+		
+		puts '-' * 20
+		puts '-' * 20
+		puts '-' * 20
+		
+		view4.zindex = 0
+		
+		puts '-' * 20
+		puts '-' * 20
+		puts '-' * 20
+		
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('C', view1.grid_cache[0][2].char)
+		assert_equal('B', view1.grid_cache[0][3].char)
+		
+		# pp view1.pp_grid_cache
+		# pp view2.pp_grid_cache
+		# pp view3.pp_grid_cache
+		# pp view4.pp_grid_cache
+	end
+	
+	def test_draw_point_add_subview
+		view1 = View.new('view1')
+		view1.is_visible = true
+		
+		assert_equal(true, view1.draw_point([0, 0], 'A'))
+		assert_equal(true, view1.draw_point([1, 0], 'B'))
+		assert_equal(true, view1.draw_point([2, 0], 'C'))
+		
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('C', view1.grid_cache[0][2].char)
+		
+		view2 = View.new('view2')
+		view2.position = Point.new(2, 0)
+		view2.is_visible = true
+		
+		assert_equal(true, view2.draw_point([0, 0], 'D'))
+		
+		view1.add_subview(view2)
+		
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('D', view1.grid_cache[0][2].char)
+	end
+	
+	def test_draw_point_remove_subview
+		view1 = View.new('view1')
+		view1.is_visible = true
+		
+		assert_equal(true, view1.draw_point([0, 0], 'A'))
+		assert_equal(true, view1.draw_point([1, 0], 'B'))
+		assert_equal(true, view1.draw_point([2, 0], 'C'))
+		
+		view2 = View.new('view2')
+		view2.position = Point.new(2, 0)
+		view2.is_visible = true
+		
+		assert_equal(true, view2.draw_point([0, 0], 'D'))
+		
+		view1.add_subview(view2)
+		
+		# pp view1.pp_grid_cache
+		
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('D', view1.grid_cache[0][2].char)
+		
+		view1.remove_subview(view2)
+		
+		# pp view1.pp_grid_cache
+		
+		assert_equal('A', view1.grid_cache[0][0].char)
+		assert_equal('B', view1.grid_cache[0][1].char)
+		assert_equal('C', view1.grid_cache[0][2].char)
+	end
+	
+	def test_draw_point_exception
+		view1 = View.new
+		
+		assert_raises(NotImplementedError){ view1.draw_point(nil, nil) }
+		assert_raises(NotImplementedError){ view1.draw_point(Point.new(0, 0), nil) }
+	end
+	
+	def test_set_grid_cache
+		content1 = ViewContent.new('A')
+		content2 = ViewContent.new('B')
+		
+		view1 = View.new('view1')
+		
+		assert_equal(true, view1.set_grid_cache(Point.new(0, 0), content1))
+		assert_equal(false, view1.set_grid_cache(Point.new(0, 0), content1))
+		assert_equal(true, view1.set_grid_cache(Point.new(0, 0), content2))
 	end
 	
 	def test_render
