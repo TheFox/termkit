d1e58e581032e79162120ceb37d0b54ec3225cd6
Christian Mayer <christian@fox21.at>
2016-10-15 13:49:55 +0200

    Print Ruby version. #4

--- bin/build_info.sh
+++ bin/build_info.sh
@@ -6,13 +6,17 @@ SCRIPT_BASEDIR=$(dirname $0)
 
 cd "${SCRIPT_BASEDIR}/.."
 
-TERMKIT_VERSION=$(ruby -I lib/termkit -rversion -e "puts %{#{TheFox::TermKit::VERSION} (#{TheFox::TermKit::DATE})}")
+RUBY_VERSION=$(ruby -e "print RUBY_VERSION")
+TERMKIT_VERSION=$(ruby -I lib/termkit -rversion -e "print %{#{TheFox::TermKit::VERSION} (#{TheFox::TermKit::DATE})}")
 
 printf "%s
 
+ruby: %s
 termkit: %s
+
+--- build ---
 id: %s
 ref: %s@%s
 stage: %s
 server: %s %s
-" "${DATE}" "${TERMKIT_VERSION}" "${CI_BUILD_ID}" "${CI_BUILD_REF}" "${CI_BUILD_REF_NAME}" "${CI_BUILD_STAGE}" "${CI_SERVER_NAME}" "${CI_SERVER_VERSION}"
+" "${DATE}" "${RUBY_VERSION}" "${TERMKIT_VERSION}" "${CI_BUILD_ID}" "${CI_BUILD_REF}" "${CI_BUILD_REF_NAME}" "${CI_BUILD_STAGE}" "${CI_SERVER_NAME}" "${CI_SERVER_VERSION}"
